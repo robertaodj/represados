de9ce7ff83aa2e919c7cb9cc9b0d2395c1d7ed51
robertaodj <robert.di.jesus@gmail.com>
2022-02-25 10:25:12 -0300

    DB2

--- conectionDB.php
+++ conectionDB.php
@@ -3,11 +3,11 @@
     try {
         /* Conexão com o banco de dados */
         $dbType = 'mysql'; /* Tipo */
-        $dbName = 'povph_31151588_represados'; /* Nome */
-        $dbPath = 'sql308.vphs.top'; /* Local */
+        $dbName = 'd9tfkt0bt6tyhm2p'; /* Nome */
+        $dbPath = 'uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306'; /* Local */
         $dbCharset = 'utf8'; /* Agrupamento */
-        $dbUser = 'povph_31151588'; /* Nome do Usuário */
-        $dbPass = 'crocodilo0210'; /* Senha do Usuário */
+        $dbUser = 'fojzkoydhafbgmja'; /* Nome do Usuário */
+        $dbPass = 'xfokw1yvp9iw4o83'; /* Senha do Usuário */
 
         $pdo = new PDO( /* Conexão com o banco de dados */
             "{$dbType}:dbname={$dbName};host={$dbPath};charset={$dbCharset}",
@@ -15,6 +15,9 @@
             $dbPass,
             [\PDO::MYSQL_ATTR_LOCAL_INFILE => true] 
         );
+
+       // mysql://fojzkoydhafbgmja:xfokw1yvp9iw4o83@uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/d9tfkt0bt6tyhm2p
+
     } catch (PDOException $e) {
         echo 'Não foi possível conectar ao banco de dados.<br>';
         echo 'Erro: ' . $e;
