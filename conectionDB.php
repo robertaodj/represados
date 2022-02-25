d2d5c87139254bb355c78d62824f023634d57ab0
robertaodj <robert.di.jesus@gmail.com>
2022-02-25 11:09:36 -0300

    BD enviado

--- conectionDB.php
+++ conectionDB.php
@@ -8,7 +8,7 @@ ini_set( 'display_errors','1');
         /* Conexão com o banco de dados */
         $dbType = 'mysql'; /* Tipo */
         $dbName = 'd9tfkt0bt6tyhm2p'; /* Nome */
-        $dbPath = 'uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306'; /* Local */
+        $dbPath = 'uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'; /* Local */
         $dbCharset = 'utf8'; /* Agrupamento */
         $dbUser = 'fojzkoydhafbgmja'; /* Nome do Usuário */
         $dbPass = 'xfokw1yvp9iw4o83'; /* Senha do Usuário */
